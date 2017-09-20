5f771bb6c2647c0a27d39c7b872011777ae1b8b0
Darron Brown <DarronBrown88@outlook.com>
2017-09-19 20:33:55 -0600

    Refactored home page

--- tlc-desktop/src/index.js
+++ tlc-desktop/src/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import * as application from './css/application.css'
+import * as docs from './css/docs.css'
+import * as toolkit from './css/toolkit.css'
 import App from './App';
+
 
 ReactDOM.render(
 	<App />, 
