17dbb367811c6e62668c85e62322af2129ceb50a
Darron Brown <DarronBrown88@outlook.com>
2017-09-26 00:35:26 -0600

    Deleted unncessary pages

--- tlc-web/src/pages/Home/HomeContent/Container/RightColumn/index.js
+++ tlc-web/src/pages/Home/HomeContent/Container/RightColumn/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Footer from './Footer';
+import Likes from './Likes';
 import Advertisement from './Advertisement';
+import { Link } from 'react-router-dom';
 
 export default class RightColumn extends Component {
 	render() {
@@ -8,8 +10,8 @@ export default class RightColumn extends Component {
 			<div {...this.props}>
 
 	      <div className="alert alert-warning alert-dismissible hidden-md-down" role="alert">
-	        <button type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	        <a className="alert-link" href="profile/index.html">Visit your profile!</a> Check your self, you aren't looking well.
+	        <button type="button" className="close" dataDismiss="alert" ariaLabel="Close"><span ariaHidden="true">&times;</span></button>
+	        <Link className="alert-link" to="/profile">Visit your profile!</Link> Check your self, you aren't looking well.
 	      </div>
 
 	      <Footer className="card card-link-list"/>
