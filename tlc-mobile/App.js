4bd5786ba2e1c9e1dd9b958eb54073f89cacbca2
Leonel Machava <leonelmachava@gmail.com>
2017-11-09 23:05:29 +0200

    Update moment settings to display condensed time reports

--- tlc-mobile/App.js
+++ tlc-mobile/App.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import moment from 'moment';
 import store from './src/store';
+
+moment.updateLocale('en', {
+  relativeTime : {
+    future: "in %s",
+    past:   "%s",
+    s  : 'now',
+    ss : '%d seconds',
+    m:  "1min",
+    mm: "%dmin",
+    h:  "1hr",
+    hh: "%dhrs",
+    d:  "1d",
+    dd: "%dd",
+    M:  "1m",
+    MM: "%dm",
+    y:  "1y",
+    yy: "%dy"
+  }
+});
 
 import AppWithNavigationState from './src/AppWithNavigationState';
 
