a783b11a6e570ce3f1703fd29ae791ae319d8257
Leonel Machava <leonelmachava@gmail.com>
2017-11-22 11:45:55 +0200

    Enhance FollowButton component

--- tlc-mobile/src/components/profile/FollowButton.js
+++ tlc-mobile/src/components/profile/FollowButton.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react';
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 export default class FollowButton extends Component {
-	state = { 
-    liked: false, 
-  }; 
-
-  _onPressBtn = () => { 
-    this.setState({ 
-			liked: !this.state.liked, 
-		}); 
-  } 
 
 	render() {
-		const likedStyles = this.state.liked ? styles.liked : null;
+		const { isFollowing, isFollowingOrUnfollowingInProgress } = this.props;
+		const likedStyles = isFollowing ? styles.following : null;
 
 		return (
 			<View style={styles.container}>
 				<TouchableOpacity
-					onPress={this._onPressBtn}  
+					onPress={this.props.onPress}
 					style={[styles.button, likedStyles]} 
 					underlayColor="#fefefe"
 				>
-					<Text style={styles.btnText}>Follow</Text>
+					{ isFollowingOrUnfollowingInProgress ?
+						<Text style={styles.btnText}>{isFollowing ? 'Unfollowing...' : 'Following...'}</Text> :
+						<Text style={styles.btnText}>{isFollowing ? 'Unfollow' : 'Follow'}</Text> }
 				</TouchableOpacity>
 			</View>
 		);
@@ -36,17 +30,19 @@ const styles = StyleSheet.create({
 		paddingRight: 8
 	},
 	button: {
-    backgroundColor: '#88b0d3',
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
     borderColor: '#fff',
     backgroundColor: 'rgba(137,178,224,0.5)',
-    borderRadius: 14
+    borderRadius: 14,
+    padding: 8,
 	},
 	btnText: {
 		color: '#fff',
 		fontWeight: 'bold',
-		margin: 8,
+	},
+	following: {
+		backgroundColor: 'rgba(200,60,60,0.5)',
 	}
 });
